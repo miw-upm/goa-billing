d638606cf44e34b99b08ef039c6c35aa661bd842
paulixx <paula.martinez.arce@alumnos.upm.es>
2026-03-20 17:22:54 +0100

    fix- enforce positive amount validation for expenses miw-upm/goa#18

--- src/main/java/es/upm/api/domain/model/Expense.java
+++ src/main/java/es/upm/api/domain/model/Expense.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import es.upm.api.domain.model.validations.PositiveBigDecimal;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,7 +25,7 @@ public class Expense {
     private UUID engagementId;
 
     @NotNull
-    @PositiveBigDecimal
+    @Positive
     private BigDecimal amount;
 
     @NotNull
