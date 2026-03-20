99e2e24edbae6a3092a3d4e97e5faaab8b80d762
paulixx <paula.martinez.arce@alumnos.upm.es>
2026-03-20 10:02:00 +0100

    add expense domain model and persistence port miw-upm/goa#18

---
+++ src/main/java/es/upm/api/domain/model/Expense.java
@@ -0,0 +1,36 @@
+package es.upm.api.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import es.upm.api.domain.model.validations.PositiveBigDecimal;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Expense {
+    private UUID id;
+
+    @NotNull
+    private UUID engagementId;
+
+    @NotNull
+    @PositiveBigDecimal
+    private BigDecimal amount;
+
+    @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate date;
+
+    @NotBlank
+    private String description;
+}
