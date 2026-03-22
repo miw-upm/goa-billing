3041c8f979d8e5a2caaaf84f8b0a991c85ebe324
aina-polomacia <aina.polomacia@alumnos.upm.es>
2026-03-22 19:38:41 +0100

    adding expense seeder miw-upm/goa#19

--- src/main/java/es/upm/api/infrastructure/mongodb/repositories/DatabaseSeederDev.java
+++ src/main/java/es/upm/api/infrastructure/mongodb/repositories/DatabaseSeederDev.java
@@ -1,5 +1,6 @@
 package es.upm.api.infrastructure.mongodb.repositories;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -9,21 +10,25 @@ import org.springframework.stereotype.Service;
 @Profile({"dev", "test"})
 public class DatabaseSeederDev {
 
+    private final ExpenseSeeder expenseSeeder;
 
-    public DatabaseSeederDev() {
-        this.deleteAllAndInitializeAndSeedDataBase();
+    public DatabaseSeederDev(ExpenseSeeder expenseSeeder) {
+        this.expenseSeeder = expenseSeeder;
     }
 
+    @PostConstruct
     public void deleteAllAndInitializeAndSeedDataBase() {
         this.deleteAllAndInitialize();
         this.seedDataBaseJava();
     }
 
     private void deleteAllAndInitialize() {
+        this.expenseSeeder.deleteAll();
         log.warn("------- Delete All -----------");
     }
 
     private void seedDataBaseJava() {
+        this.expenseSeeder.seedDatabase();
         log.warn("------- Initial Load from JAVA ---------------------------------------------------------------");
     }
 
