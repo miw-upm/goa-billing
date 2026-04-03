68a6ebc1b377841a9435ba89e4daacbb2d389893
paulixx <paula.martinez.arce@alumnos.upm.es>
2026-04-03 14:07:02 +0200

    add mongodb repository queries for income date filters miw-upm/goa#105

--- src/main/java/es/upm/api/infrastructure/mongodb/repositories/IncomeRepository.java
+++ src/main/java/es/upm/api/infrastructure/mongodb/repositories/IncomeRepository.java
@@ -4,9 +4,12 @@ import es.upm.api.infrastructure.mongodb.entities.IncomeEntity;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
 public interface IncomeRepository extends MongoRepository<IncomeEntity, UUID> {
     List<IncomeEntity> findByEngagementId(UUID engagementId, Sort sort);
+    List<IncomeEntity> findByDate(LocalDate date, Sort sort);
+    List<IncomeEntity> findByEngagementIdAndDate(UUID engagementId, LocalDate date, Sort sort);
 }
