155a68a93635aeca77abdb7fe535be55c1adf114
paulixx <paula.martinez.arce@alumnos.upm.es>
2026-04-03 14:00:54 +0200

    support income filters through criteria in domain service miw-upm/goa#105

--- src/main/java/es/upm/api/domain/model/IncomeFindCriteria.java
+++ src/main/java/es/upm/api/domain/model/IncomeFindCriteria.java
@@ -1,4 +1,20 @@
 package es.upm.api.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class IncomeFindCriteria {
+    private UUID engagementId;
+    private LocalDate date;
+
+    public boolean isEmpty() {
+        return this.engagementId == null && this.date == null;
+    }
 }
